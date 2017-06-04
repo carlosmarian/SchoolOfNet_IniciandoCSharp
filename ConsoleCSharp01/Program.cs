09c98dbd9ab443569a982947dc62c36849d60911
Carlos H. M <carlosmarian@gmail.com>
2017-06-04 18:38:16 -0300

    chapter 'enum'

--- ConsoleCSharp01/Program.cs
+++ ConsoleCSharp01/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace ConsoleCSharp01
 {
@@ -6,22 +7,66 @@ namespace ConsoleCSharp01
     {
         static void Main()
         {
-            //MinhaData m = new MinhaData();
-            //m.dataAtual
 
-            Pessoa pessoa = new Pessoa();
-            pessoa.Nome = "João";
-            pessoa.SobreNome = "Silva";
-            pessoa.Sexo = Sexo.Masculino;
-            pessoa.Idade = 20;
+            //Queue fila = new Queue();
 
-            pessoa.Idade = -1;
+            //fila.Enqueue("Carlos");
+            //fila.Enqueue("Maria");
+            //fila.Enqueue("Pedro");
 
-            pessoa.Falar(string.Format("Olá, sou o {0} {1} e tenho {2} anos ", pessoa.Nome, pessoa.SobreNome, pessoa.Idade));
+            //while(fila.Count > 0)
+            //{
+            //    var valor = fila.Dequeue();
+            //    Console.WriteLine(valor);
+            //}
 
-            pessoa = new Pessoa();
+            //Stack pilha = new Stack();
 
-            pessoa.Falar(string.Format("Olá, sou o {0} {1} e tenho {2} anos ", pessoa.Nome, pessoa.SobreNome, pessoa.Idade));
+            //pilha.Push("Carlos 1");
+            //pilha.Push("Maria 2");
+            //pilha.Push("Pedro 3");
+            //pilha.Push("João 4");
+
+            //while(pilha.Count > 0)
+            //{
+            //    var valor = pilha.Pop();
+            //    Console.WriteLine(valor);
+            //}
+
+            ArrayList lista = new ArrayList();
+
+            lista.Insert(0, "Carlos 1");            
+            lista.Insert(1, "Joao 2");
+            lista.Insert(2, "Maria 3");
+            lista.Insert(3, "Pedro 4");
+            lista.Insert(2, "MARIA 3");
+
+            for (int i = 0; i < lista.Count; i++)
+            {
+                var item = lista[i];
+
+                Console.WriteLine(item);
+
+            }
+
+
+
+            ////MinhaData m = new MinhaData();
+            ////m.dataAtual
+
+            //Pessoa pessoa = new Pessoa();
+            //pessoa.Nome = "João";
+            //pessoa.SobreNome = "Silva";
+            //pessoa.Sexo = Sexo.Masculino;
+            //pessoa.Idade = 20;
+
+            //pessoa.Idade = -1;
+
+            //pessoa.Falar(string.Format("Olá, sou o {0} {1} e tenho {2} anos ", pessoa.Nome, pessoa.SobreNome, pessoa.Idade));
+
+            //pessoa = new Pessoa();
+
+            //pessoa.Falar(string.Format("Olá, sou o {0} {1} e tenho {2} anos ", pessoa.Nome, pessoa.SobreNome, pessoa.Idade));
             Console.ReadLine();
         }
     }
